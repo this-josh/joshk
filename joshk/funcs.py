3235379101f3e9e95f6eb0a4b06e3280b0683edc
Josh <joshuaarkirk@hotmail.co.uk>
2021-10-27 12:14:57 +0100

    *   Improved error messages and gave more useful feedback to the user

--- joshk/funcs.py
+++ joshk/funcs.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 import re
+import click
 
 EXAMPLE_TEXT_IN = """@article{Pecci2019,
    abstract = {This manuscript investigates the problem of optimal placement of control valves in water supply networks, where the objective is to minimize average zone pressure. The problem formulation results in a nonconvex mixed integer nonlinear program (MINLP). Due to its complex mathematical structure, previous literature has solved this nonconvex MINLP using heuristics or local optimization methods, which do not provide guarantees on the global optimality of the computed valve configurations. In our approach, we implement a branch and bound method to obtain certified bounds on the optimality gap of the solutions. The algorithm relies on the solution of mixed integer linear programs, whose formulations include linear relaxations of the nonconvex hydraulic constraints. We investigate the implementation and performance of different linear relaxation schemes. In addition, a tailored domain reduction procedure is implemented to tighten the relaxations. The developed methods are evaluated using two benchmark water supply networks and an operational water supply network from the UK. The proposed approaches are shown to outperform state-of-the-art global optimization solvers for the considered benchmark water supply networks. The branch and bound algorithm converges to good quality feasible solutions in most instances, with bounds on the optimality gap that are comparable to the level of parameter uncertainty usually experienced in water supply network models.},
@@ -88,6 +89,11 @@ def make_request(properties):
     }
     data = json.dumps(data)
     r = requests.post(url, data=data, headers=headers)
-    print(r.status_code)
-    if r.status_code != 200:
-        print(r.content)
+
+    title = properties["properties"]["Name"]["title"][0]["text"]["content"]
+    if r.status_code == 200:
+        click.echo(f"Successfully added {title} to Notion")
+    else:
+        click.echo(f"Failed to upload to Notion, error message below:")
+        click.echo(f"Error code {r.status_code}")
+        click.echo(r.content)
--- release_notes.md
+++ release_notes.md
@@ -1,5 +1,9 @@
 # Release notes
 
+## 0.1.3
+
+*   Improved error messages and gave more useful feedback to the user
+
 ## 0.1.2
 
 *   There seems to be an issue with click in `0.1.1`, trying again
