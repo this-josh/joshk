02aaacd49c620a3b2a99459e41d25c944dbeea6d
Josh <joshuaarkirk@hotmail.co.uk>
2021-10-27 12:03:38 +0100

    renamed methods and added descriptions

--- joshk/__main__.py
+++ joshk/__main__.py
@@ -1,15 +1,15 @@
 import click
-from bibtex_to_notion.funcs import txt_to_json, to_properties, make_request
+from joshk.funcs import txt_to_json, to_properties, make_request
 
 
 @click.command()
-@click.option("--bibtex", help="The person to greet.")
-def notion_bibtext(bibtex):
-    """Simple program that greets NAME for a total of COUNT times."""
+@click.option("--bibtex", help="The bibtex to add to notion")
+def bibtex_to_notion(bibtex):
+    """A method for inserting bibtex entries into notion."""
     bibtex_json = txt_to_json(bibtex)
     properties = to_properties(bibtex_json)
     make_request(properties)
 
 
 if __name__ == "__main__":
-    notion_bibtext()
+    bibtex_to_notion()
